eb04803b5f56187a87934ed3a67d4f174d36c2cd
kalinaea.24 <elizaveta.kalina.07@gmail.com>
2023-01-28 10:53:26 +0300

    documented

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,16 @@
 import app.Application;
 import io.github.humbleui.jwm.App;
+/**
+ * Главный класс
+ */
 public class Main {
+    /**
+     * Главный метод
+     * @param args - аргументы командной строки
+     */
     public static void main(String[] args) {
         App.start(Application::new);
+
 
     }
 }
--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -4,13 +4,28 @@ import io.github.humbleui.jwm.*;
 
 import java.util.function.Consumer;
 
+/**
+ * Класс приложения
+ */
 public class Application implements Consumer<Event> {
+    /**
+     * Окно приложения
+     */
     private final Window window;
+    /**
+     * Конструктор класса
+     */
     public Application() {
         window = App.makeWindow();
         window.setEventListener(this);
         window.setVisible(true);
     }
+
+    /**
+     *
+     * @param e the input argument
+     */
+    @Override
     public void accept(Event e) {
         if (e instanceof EventWindowClose) {
             App.terminate();
